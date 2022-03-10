0d953e53a49d0d3afec662ebe6f504c4b056bfcc
ZHENK <chengzhenyang@gmail.com>
2022-03-11 11:55:54 +1300

    Setup MUI, Router and basic project structure. (#11)
    * Added Material UI to project
    
    * Fix prettier and eslint settings for IDE
    
    * Add first page and components with unit tests
    
    Co-authored-by: DanilLinkov <danil.linkov@gmail.com>

---
+++ src/components/Example/Example.jsx
@@ -0,0 +1,8 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+function Example() {
+  return <Typography>This is an example component.</Typography>;
+}
+
+export default Example;
---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+import Example from '../../components/Example';
+
+function Dashboard() {
+  return (
+    <>
+      <Typography>Welcome to the Dashboard</Typography>
+      <Example />
+      <Example />
+    </>
+  );
+}
+
+export default Dashboard;
