a8bd199639f38aaeb474605c7646e202d74822b3
Kirsty Gong <kirstygong23@gmail.com>
2022-03-12 21:45:16 +1300

    test: added test to test avatar is rendered

--- src/components/ProfileBadge/_tests_/ProfileBadge.test.jsx
+++ src/components/ProfileBadge/_tests_/ProfileBadge.test.jsx
@@ -7,6 +7,7 @@ describe('Render Active ProfileBadge', () => {
   test('ProfileBadge test', () => {
     render(<ProfileBage active />);
     expect(screen.getByTestId('active'));
+    expect(screen.getByTestId('avatar'));
   });
 });
 
@@ -14,5 +15,6 @@ describe('Render Offline ProfileBadge', () => {
   test('ProfileBadge test', () => {
     render(<ProfileBage active={false} />);
     expect(screen.getByTestId('offline'));
+    expect(screen.getByTestId('avatar'));
   });
 });
