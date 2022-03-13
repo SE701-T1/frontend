34e84dfc15330dcf84a4f3f409991fa4c2e41536
Kirsty Gong <kirstygong23@gmail.com>
2022-03-13 17:01:47 +1300

    feat: course card component

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -1,37 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { CardActionArea, Grid } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import GroupsIcon from '@mui/icons-material/Groups';
 
 import styles from './CourseCard.module.css';
 
-function CourseCard() {
+function CourseCard({ courseName, semesterNumber, numbOfStudents, numOfBuddies }) {
+  const year = new Date().getFullYear();
+  const [selected, setSelected] = useState(true);
+
+  const cardContent = (
+    <CardContent>
+      <Grid container direction="column" spacing={3}>
+        <Grid item>
+          <Typography className={styles.courseNameText} variant="h4" fontWeight={550}>
+            {courseName}
+          </Typography>
+          <Typography className={styles.semesterText} fontSize={14}>
+            SEMESTER {semesterNumber} {year}
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Grid container direction="row" spacing={1}>
+            <Grid item>
+              <PersonIcon className={styles.icons} />
+            </Grid>
+            <Grid item>
+              <Typography alignItems="end" variant="subtitle2">
+                <strong>{numbOfStudents} Members</strong>
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid container direction="row" spacing={1}>
+            <Grid item>
+              <GroupsIcon className={styles.icons} />
+            </Grid>
+            <Grid item>
+              <Typography alignItems="end" variant="subtitle2">
+                <strong>{numOfBuddies} Buddies</strong>
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </CardContent>
+  );
+
+  console.log(selected);
   return (
     <Box>
       <Card className={styles.root}>
-        <fragment>
-          <CardContent>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-              Word of the Day
-            </Typography>
-            <Typography variant="h5" component="div">
-              hi
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              adjective
-            </Typography>
-            <Typography variant="body2">
-              well meaning and kindly.
-              <br />a benevolent smile
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Learn More</Button>
-          </CardActions>
-        </fragment>
+        <CardActionArea onClick={() => setSelected(!selected)}>{cardContent}</CardActionArea>
       </Card>
     </Box>
   );
