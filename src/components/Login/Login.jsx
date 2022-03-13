cb6e82826323c23deb50fc9d31389cdd83443689
Dhvani Shah <73872595+dshah1901@users.noreply.github.com>
2022-03-13 23:41:23 +1300

    Implemented the custom login button

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import GoogleLogin from "react-google-login";
-
+import { FcGoogle } from 'react-icons/fc'
+import styles from './Login.module.css'
 
 const clientId = "158309441002-q8q49tjicngt1tp6p9t7ecvdrn9ar78j.apps.googleusercontent.com";
 
@@ -18,6 +19,15 @@ export default function Login(){
         <div>
             <GoogleLogin
             clientId={clientId}
+            render={renderProps => (
+                <button className={`${styles.loginButton}`} 
+                type="button"
+                onClick={renderProps.onClick} 
+                disabled={renderProps.disabled}>
+                    <FcGoogle size={22} />
+                    <div className={`${styles.loginText}`}>Sign Up Using Google</div>
+                </button>
+              )}
             onSuccess = {onSuccess}
             onFailure = {onFailure}
             cookiePolicy= "single_host_origin"
