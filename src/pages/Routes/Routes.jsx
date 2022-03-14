d2db6cdf3088011c730a53dc2e60b719dd073127
Arathi Biju <62319771+arathibiju@users.noreply.github.com>
2022-03-14 17:57:02 +1300

    removed route that was used for testing

--- src/pages/Routes/Routes.jsx
+++ src/pages/Routes/Routes.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Route, Routes as Switch } from 'react-router-dom';
 import Dashboard from '../Dashboard';
-import UploadPage from '../../components/Timetable';
 
 function Routes() {
   return (
     <Switch>
       <Route path="/" element={<Dashboard />} />
-      <Route path="/upload" element={<UploadPage />} />
     </Switch>
   );
 }
