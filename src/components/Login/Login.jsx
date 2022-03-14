895797ff185a5e2335624e2504b7102ff90e536f
Dhvani Shah <73872595+dshah1901@users.noreply.github.com>
2022-03-14 14:16:40 +1300

    Added props to login and some comments

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,18 +2,30 @@ import React from "react";
 import GoogleLogin from "react-google-login";
 import { FcGoogle } from 'react-icons/fc'
 import styles from './Login.module.css'
+/**
+ * The Login button component can be used to authenticate the user using a Google oAuth. 
+ * The action of what happens if the user is succesfully authenticates (onSuccess) and 
+ * if the authentication fails (onFailure) are passed as props which have the following structure:
+ * 
+ * Example of onSuucess prop that would print the profileObj and tokenObj on the console
+ * const onSuccess = (res) => {
+ *      console.log(res.profileObj)
+ *      console.log(res.tokenObj)
+ * }
+ * 
+ * The "react-google-login" package is used that provides simple mechanisms to add the Google login button.
+ * For more impormation visit: "https://www.npmjs.com/package/react-google-login"
+ * 
+ * The clientId was obtained from Google developer console, that is used to identify the application for authentication details..
+ * The isSignedIn prop is used to ensure the user stays logged in.
+ * The render prop used is used to set up a customized Google login button.
+ * 
+ * The "react-icons" package was used to use the google icon "FcGoogle" on the customized sign in button.
+ */
 
 const clientId = "158309441002-q8q49tjicngt1tp6p9t7ecvdrn9ar78j.apps.googleusercontent.com";
 
-export default function Login(){
-
-    const onSuccess = (res) => {
-        console.log("LOGIN SUCCESS! ", res.tokenObj);
-    }
-
-    const onFailure = (res) => {
-        console.log("LOGIN FAILED! res: ", res);
-    }
+export default function Login({onSuccess, onFailure}){
 
     return(
         <div>
