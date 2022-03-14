8cd822f07f436183d32310be6ec718fa9b6ae841
Kirsty Gong <kirstygong23@gmail.com>
2022-03-14 15:38:27 +1300

    feat: right corner styling for selected course

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -6,12 +7,43 @@ import Typography from '@mui/material/Typography';
 import { CardActionArea, Grid } from '@mui/material';
 import PersonIcon from '@mui/icons-material/Person';
 import GroupsIcon from '@mui/icons-material/Groups';
+import CheckCircleOutlinedIcon from '@mui/icons-material/CheckCircleOutlined';
 
 import styles from './CourseCard.module.css';
 
+const SelectedCard = styled(Card)(() => ({
+  border: '4px solid #8896ac',
+  borderRadius: '8%',
+
+  position: 'relative',
+  '&::before,&::after': {
+    content: "''",
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    borderColor: 'transparent',
+    borderStyle: 'solid',
+  },
+  '&::before': {
+    borderWidth: '4px',
+    borderRightColor: 'transparent',
+    borderTopColor: 'transparent',
+  },
+  '&::after': {
+    borderRadius: '0%',
+    borderWidth: '30px',
+    borderRightColor: '#8896ac' /* color of the triangle */,
+    borderTopColor: '#8896ac' /* color of the triangle */,
+  },
+}));
+
+const StyledCard = styled(Card)(() => ({
+  borderRadius: '5%',
+}));
+
 function CourseCard({ courseName, semesterNumber, numbOfStudents, numOfBuddies }) {
   const year = new Date().getFullYear();
-  const [selected, setSelected] = useState(true);
+  const [selected, setSelected] = useState(false);
 
   const cardContent = (
     <CardContent>
@@ -50,12 +82,20 @@ function CourseCard({ courseName, semesterNumber, numbOfStudents, numOfBuddies }
     </CardContent>
   );
 
-  console.log(selected);
   return (
-    <Box>
-      <Card className={styles.root}>
-        <CardActionArea onClick={() => setSelected(!selected)}>{cardContent}</CardActionArea>
-      </Card>
+    <Box className={styles.root}>
+      {selected ? (
+        <Box className={styles.root}>
+          <SelectedCard className={styles.card}>
+            <CardActionArea onClick={() => setSelected(!selected)}>{cardContent}</CardActionArea>
+          </SelectedCard>
+          <CheckCircleOutlinedIcon className={styles.tickIcon} />
+        </Box>
+      ) : (
+        <StyledCard className={styles.card}>
+          <CardActionArea onClick={() => setSelected(!selected)}>{cardContent}</CardActionArea>
+        </StyledCard>
+      )}
     </Box>
   );
 }
