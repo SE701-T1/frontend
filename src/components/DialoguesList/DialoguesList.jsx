871bddfd7d3009d43467b5e1151619c38cb009a6
SV Singh <ssin610@aucklanduni.ac.nz>
2022-03-16 10:04:13 +1300

    Conversation dialogues list component (#69)
    * Added user search box
    
    * Implemented basic unit tests and some styling
    
    * Added additional unit tests and event handlers
    
    * Added comments
    
    * Added props and simplications
    
    * Added prettier
    
    * removed unused import
    
    * Added dialogues list component
    
    * Refactored dialogueslist component
    
    * Added unit tests and further refactored
    
    * Update src/components/DialoguesList/DialoguesList.jsx
    
    Co-authored-by: ZHENK <chengzhenyang@gmail.com>
    
    * Added comments
    
    Co-authored-by: ZHENK <chengzhenyang@gmail.com>

---
+++ src/components/DialoguesList/DialoguesList.jsx
@@ -0,0 +1,62 @@
+import React, { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import styles from './DialoguesList.module.css';
+import MessageBubble from '../MessageBubble/MessageBubble';
+
+// TODO: display the timestamp of messages
+
+/**
+ * DialoguesList component displays messages in the form of MessageBubbles between users.
+ * This component takes in an array of objects (messages) which contain properties sender id, text, 
+ * and content type.
+ * Example:
+ *  {
+ *   sender: 'id3333',
+ *   text: 'hello there',
+ *   contentType: 'text',
+ *  }
+ */
+function DialoguesList({ messages, currentUser, senderAvatar }) {
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  // scroll to bottom when a new message comes
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  // render the message bubbles from the messages prop
+  return (
+    <div className={styles.root}>
+      {messages.map(({ text, sender, contentType }, key) => (
+        <MessageBubble
+          text={text}
+          sender={sender}
+          currentUser={currentUser}
+          senderAvatar={senderAvatar}
+          contentType={contentType}
+          // eslint-disable-next-line react/no-array-index-key
+          key={key}
+        />
+      ))}
+      <div className={styles.bottom} ref={messagesEndRef} />
+    </div>
+  );
+}
+
+DialoguesList.propTypes = {
+  currentUser: PropTypes.string.isRequired,
+  senderAvatar: PropTypes.string.isRequired,
+  messages: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string.isRequired,
+      sender: PropTypes.string.isRequired,
+      contentType: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default DialoguesList;
