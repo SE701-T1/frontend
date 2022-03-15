822e3f4f27fd4ab0548f7c5805ab64477453d01f
SV Singh <ssin610@aucklanduni.ac.nz>
2022-03-15 19:18:48 +1300

    Refactored dialogueslist component

--- src/components/DialoguesList/DialoguesList.jsx
+++ src/components/DialoguesList/DialoguesList.jsx
@@ -1,26 +1,51 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import * as React from 'react';
-
+import React, { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import styles from './DialoguesList.module.css';
+import MessageBubble from '../MessageBubble/MessageBubble';
 
-export default function DialoguesList({ messages }) {
-  const messagesEndRef = React.useRef(null);
+// TODO: display the timestamp of messages
+function DialoguesList({ messages, currentUser, senderAvatar, contentType }) {
+  const messagesEndRef = useRef(null);
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
-  React.useEffect(() => {
+  // scroll to bottom when a new message comes
+  useEffect(() => {
     scrollToBottom();
   }, [messages]);
 
+  // render the message bubbles from the messages prop
   return (
     <div className={styles.root}>
-      {messages.map((component, key) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <div key={key}>{component}</div>
+      {messages.map(({ text, sender }, key) => (
+        <MessageBubble
+          text={text}
+          sender={sender}
+          currentUser={currentUser}
+          senderAvatar={senderAvatar}
+          contentType={contentType}
+          // eslint-disable-next-line react/no-array-index-key
+          key={key}
+        />
       ))}
       <div className={styles.bottom} ref={messagesEndRef} />
     </div>
   );
 }
+
+DialoguesList.propTypes = {
+  currentUser: PropTypes.string.isRequired,
+  senderAvatar: PropTypes.string.isRequired,
+  messages: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string.isRequired,
+      sender: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  contentType: PropTypes.string.isRequired,
+};
+
+export default DialoguesList;
