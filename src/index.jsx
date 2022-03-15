046a4fd675d20b16e85b1272cff926f4e2ede32d
Shrey Tailor <39754888+shreytailor@users.noreply.github.com>
2022-03-15 15:44:35 +1300

    Moved AuthContextProvider from index to App.

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { AuthContextProvider } from './context/authContext';
 import { ServiceWorkerProvider } from './context/serviceWorkerContext';
 
 ReactDOM.render(
   <React.StrictMode>
     <ServiceWorkerProvider>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
+      <App />
     </ServiceWorkerProvider>
   </React.StrictMode>,
   document.getElementById('root'),
