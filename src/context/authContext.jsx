47b9c76a53edcb2938cdae62eb7baa639e7a6dbc
Shrey Tailor <39754888+shreytailor@users.noreply.github.com>
2022-03-15 14:54:36 +1300

    Added comments explaining the component.

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -4,10 +4,19 @@ import createPersistedState from 'use-persisted-state';
 const useJwtState = createPersistedState('jwt');
 export const AuthContext = React.createContext({});
 
+/**
+ * The main application is wrapped around this {@link AuthContextProvider} so that other modules of
+ * the application are able to access properties related to User authentication e.g. current JWT,
+ * authentication status and helper function to log the user out.
+ *
+ * A library called 'use-persisted-state' is being used to store the token in the local storage of
+ * the browser.
+ */
 export function AuthContextProvider({ children }) {
   const [jwt, setJwt] = useJwtState();
   const [authenticated, setAuthenticated] = useState(false);
 
+  // Clears the JWT, which triggers the useEffect() below to set authenticated to false.
   function logout() {
     setJwt('');
   }
@@ -18,6 +27,7 @@ export function AuthContextProvider({ children }) {
       return;
     }
 
+    // Triggering a backend call to verify the integrity of the current JWT.
     const response = await fetch(`${process.env.API_HOST}:${process.env.API_PORT}/login`, {
       method: 'GET',
       headers: new Headers({
@@ -37,6 +47,6 @@ export function AuthContextProvider({ children }) {
     setJwt('');
   }, [jwt]);
 
-  const context = useMemo(() => ({ setJwt, authenticated, logout }), [authenticated]);
+  const context = useMemo(() => ({ jwt, setJwt, authenticated, logout }), [jwt, authenticated]);
   return <AuthContext.Provider value={context}>{children}</AuthContext.Provider>;
 }
