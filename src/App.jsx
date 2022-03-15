92ed86ee487a09839fe895e368247c96eae746fb
Shrey Tailor <shreytailor@canva.com>
2022-03-15 15:56:59 +1300

    Feature: Global authentication context (#61)
    * Added use-persisted-state dependency.
    
    * Created a JWT store using context.
    
    * Added comments explaining the component.
    
    * Changing the verification endpoint to /verify.
    
    * Moved AuthContextProvider from index to App.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import { ThemeProvider } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './pages/Routes';
 import theme from './components/Theme';
+import { AuthContextProvider } from './context/authContext';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </ThemeProvider>
+    <AuthContextProvider>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </ThemeProvider>
+    </AuthContextProvider>
   );
 }
 
