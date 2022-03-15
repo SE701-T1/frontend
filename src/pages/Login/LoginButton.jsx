a6c30ccca47fd9e4cb160cb42e68b63edde58d5c
Dhvani Shah <73872595+dshah1901@users.noreply.github.com>
2022-03-15 16:09:44 +1300

    Updated the comments

--- src/pages/Login/LoginButton.jsx
+++ src/pages/Login/LoginButton.jsx
@@ -5,32 +5,31 @@ import { FcGoogle } from 'react-icons/fc'
 import styles from './LoginButton.module.css'
 
 /**
- * The Login button component can be used to authenticate the user using a Google oAuth. 
- * The action of what happens if the user is succesfully authenticates (onSuccess) and 
- * if the authentication fails (onFailure) are passed as props which have the following structure:
+ * The Login button component can be used to authenticate the user using a Google OAuth. 
+ * The action of what happens if the user is successfully authenticated(onSuccess) and 
+ * if the authentication fails(onFailure), are passed as props that have the following structure:
  * 
- * Example of onSuucess prop that would print the profileObj and tokenObj on the console
+ * Example of onSuccess prop that would print the profileObj and tokenObj on the console
  * const onSuccess = (res) => {
  *      console.log(res.profileObj)
  *      console.log(res.tokenObj)
  * }
  * 
  * The "react-google-login" package is used that provides simple mechanisms to add the Google login button.
- * For more impormation visit: "https://www.npmjs.com/package/react-google-login"
+ * For more imformation visit: "https://www.npmjs.com/package/react-google-login"
  * 
- * The clientId was obtained from Google developer console, that is used to identify the application for authentication details..
+ * The clientId was obtained from the Google developer console, which is used to identify the application for authentication details.
  * The isSignedIn prop is used to ensure the user stays logged in.
- * The render prop used is used to set up a customized Google login button.
+ * The render prop is used to set up a customized Google login button.
  * 
- * The "react-icons" package was used to use the google icon "FcGoogle" on the customized sign in button.
+ * The "react-icons" package is used to get the google icon "FcGoogle" on the customized sign in button.
+ * The "react-dotenv" package is used to load the client ID dynamically.
  * 
  * After clicking on the button, a window that allows the user to sign in with Google opens up.
- * After signing up the user will be redirected to the same page they login in from (i.e. http://localhost:3000/), 
+ * After signing up, the user will be redirected to the same page they login in from (i.e. http://localhost:3000/), 
  * which can be changed from Google Developer Console later. 
- * 
  */
 
-// const clientId = "158309441002-q8q49tjicngt1tp6p9t7ecvdrn9ar78j.apps.googleusercontent.com";
 const clientId = env.CLIENT_ID;
 
 export default function LoginButton({onSuccess, onFailure}){
