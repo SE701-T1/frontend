b18998ba210864fdebe3145b0a38bb339bc676d8
Dhvani Shah <73872595+dshah1901@users.noreply.github.com>
2022-03-15 19:53:14 +1300

    Added some css

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import LoginButton from './LoginButton'
 import Carousel from '../../components/Carousel/Carousel'
+import styles from './Login.module.css'
 
 export default function Login() {
     const data = 
@@ -8,24 +9,24 @@ export default function Login() {
         {
             id:1,
             title: "Online classes robbing you of the experince of making friends?",
-            description: "Get partners to study with! Even Batman needs Justice league to defeat DarkSeid, just saying....Join BuddyMaker!"
+            description: "Get partners to study with! Even Batman needs Justice league to defeat DarkSeid, just saying!"
         },
         {
             id:2,
             title: "Collaborative learning is better than solo learning!",
-            description: "Statistics suggest that 78% of people are able to perform well in their courses if they learn with a community. Join BuddyMaker!"
+            description: "Statistics suggest that 78% of people are able to perform well in their courses if they learn with a community."
         },
         {
             id:3,
-            title: "Get teammates to study your courses with ollaborative learning is better than solo learning!",
-            description: "Or find awesome people to form a super secret rock band with ;)  Join BuddyMaker!"
+            title: "Get teammates to study your courses with collaborative learning is better than solo learning!",
+            description: "Or find awesome people to form a super secret rock band with ;)"
         }
     ]
 
     return (
-    <div>
-        <Carousel data={data}/>
-        <LoginButton/>        
+    <div className = {`${styles.container}`}>
+        <Carousel data={data} className = {`${styles.carousel}`}/>
+        <LoginButton className = {`${styles.login}`}/>        
     </div>
     )
 }
