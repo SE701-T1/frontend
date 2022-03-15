abef47a0d01027ffbf4e2f2887e29776cf24541c
Dhvani Shah <73872595+dshah1901@users.noreply.github.com>
2022-03-16 00:01:53 +1300

    Changed Routes.jsx to previous version

--- src/pages/Routes/Routes.jsx
+++ src/pages/Routes/Routes.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 import { Route, Routes as Switch } from 'react-router-dom';
 
 import Example from '../../components/Example';
+import Nav from '../../components/NavBar';
 import Dashboard from '../Dashboard';
-import Login from '../Login/Login';
 
 function Routes() {
   return (
-    
+    <Nav>
       <Switch>
         <Route path="/courses" element={<Example />} />
         <Route path="/find-matches" element={<Example />} />
         <Route path="/chat" element={<Example />} />
         <Route path="/accounts" element={<Example />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Dashboard />} />
       </Switch>
-    
+    </Nav>
   );
 }
 export default Routes;
