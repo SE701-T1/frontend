a162e31b1c6399420c7b869e0d4a8c62a5ba5e11
colaMeowSY <stephy99y@gmail.com>
2022-03-16 19:20:39 +1300

    Improve css and font style

--- src/components/ChatHeader/ChatHeader.jsx
+++ src/components/ChatHeader/ChatHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IconButton } from '@mui/material';
+import { IconButton,Typography,Box } from '@mui/material';
 import PropTypes from 'prop-types';
 import CallIcon from '@mui/icons-material/Call';
 import ProfileBadge from '../ProfileBadge/ProfileBadge';
@@ -12,23 +12,23 @@ import styles from './ChatHeader.module.css';
  */
 function ChatHeader({ active, name, callBuddy }) {
   return (
-    <div className={styles.chatHeader}>
-      <div className={styles.userDetails}>
+    <Box className={styles.chatHeader}>
+      <Box className={styles.userDetails}>
         <ProfileBadge active={active} name={name} />
-        <div className={styles.detailsText}>
-          <p
+        <Box>
+          <Typography variant='h6' fontWeight={600} lineHeight={1}
             className={`${styles.headerText} ${styles.buddyName} ${
               active ? styles.onlinePadding : styles.offlinePadding
             }`}>
             {name}
-          </p>
-          <p className={`${styles.headerText} ${styles.status}`}>{active ? 'Active now' : ' '}</p>
-        </div>
-      </div>
+          </Typography>
+          <Typography variant='body2' fontWeight={500} lineHeight={1.5} className={`${styles.headerText} ${styles.status}`}>{active ? 'Active now' : ' '}</Typography>
+        </Box>
+      </Box>
       <IconButton onClick={callBuddy} data-testid="call-buddy">
         <CallIcon fontSize="large" />
       </IconButton>
-    </div>
+    </Box>
   );
 }
 
