20405c44ae498ec08ad89fc364f1675809b9075d
colaMeowSY <stephy99y@gmail.com>
2022-03-16 19:25:09 +1300

    Format code and update snapshot test

--- src/components/ChatHeader/ChatHeader.jsx
+++ src/components/ChatHeader/ChatHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IconButton,Typography,Box } from '@mui/material';
+import { IconButton, Typography, Box } from '@mui/material';
 import PropTypes from 'prop-types';
 import CallIcon from '@mui/icons-material/Call';
 import ProfileBadge from '../ProfileBadge/ProfileBadge';
@@ -16,13 +16,22 @@ function ChatHeader({ active, name, callBuddy }) {
       <Box className={styles.userDetails}>
         <ProfileBadge active={active} name={name} />
         <Box>
-          <Typography variant='h6' fontWeight={600} lineHeight={1}
+          <Typography
+            variant="h6"
+            fontWeight={600}
+            lineHeight={1}
             className={`${styles.headerText} ${styles.buddyName} ${
               active ? styles.onlinePadding : styles.offlinePadding
             }`}>
             {name}
           </Typography>
-          <Typography variant='body2' fontWeight={500} lineHeight={1.5} className={`${styles.headerText} ${styles.status}`}>{active ? 'Active now' : ' '}</Typography>
+          <Typography
+            variant="body2"
+            fontWeight={500}
+            lineHeight={1.5}
+            className={`${styles.headerText} ${styles.status}`}>
+            {active ? 'Active now' : ' '}
+          </Typography>
         </Box>
       </Box>
       <IconButton onClick={callBuddy} data-testid="call-buddy">
