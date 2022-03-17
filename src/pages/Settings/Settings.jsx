03304cd2707d0631c9470caf7882c027f381bd86
Dhvani Shah <73872595+dshah1901@users.noreply.github.com>
2022-03-17 19:10:06 +1300

    Added documentation

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -3,16 +3,26 @@ import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import styles from './Settings.module.css'
 
+/**
+ * The settings page displays a toggle button that the user can click on.If the off option is selected, 
+ * then value of newPairing value is set to "off" and it set to "on" otherwise.
+ * 
+ * Default value of pairing will be set to on.
+ * 
+ * More information about the toggle button can be found here: https://mui.com/components/toggle-button/
+ * 
+ */
+
 export default function Settings() {
-  const [pairing, setPairing] = React.useState('off');
+  const [pairing, setPairing] = React.useState('on');
 
   const handleChange = (event, newPairing) => {
     setPairing(newPairing);
-    console.log(newPairing);
   };
 
   return (
     <ToggleButtonGroup
+      className={`${styles.container}`}
       value={pairing}
       exclusive
       onChange={handleChange} 
