96c33afc469b02bc1a9854c51b85de5dfe460923
uoa-lzha989 <68832471+uoa-lzha989@users.noreply.github.com>
2022-03-19 00:18:56 +1300

    Utilisation of css styles and adjustment of margins

--- src/components/PopUp/PopUp.jsx
+++ src/components/PopUp/PopUp.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import { Button, Dialog, Typography } from '@mui/material';
-// import styles from './PopUp.module.css';
 import PeopleIcon from '@mui/icons-material/People';
+import styles from './PopUp.module.css';
 import ProfileBadge from '../ProfileBadge/ProfileBadge';
 
 // const [open, setOpen] = React.useState(true);
@@ -13,24 +13,36 @@ function PopUp({ name, buddyNumber, open }) {
   // };
 
   const sharedCourses = ['course1', 'course2'];
-  const courses = sharedCourses.join(", ");
+  const courses = sharedCourses.join(', ');
 
   return (
-    <Dialog open={open}>
-      <div>
-        <ProfileBadge size="300px" />
+    <Dialog open={open} fullWidth>
+      <div className={styles.icon}>
+        <ProfileBadge size="250px" marginTop="20px" />
+      </div>
+      <div className={styles.title}>
         <h1> {name} </h1>
-        <Typography>also takes {courses}</Typography>
       </div>
-      <div>
-        <PeopleIcon />
-        <Typography>{buddyNumber} Buddies</Typography>
+      <div className={styles.courses}>
+        <Typography>Also takes {courses.toUpperCase()}</Typography>
       </div>
-      <div>
-        <Button variant="contained">say hi</Button>
+      <div className={styles.buddies}>
+            <PeopleIcon style={{ marginRight: '5px'}}/>
+        {buddyNumber} Buddies
       </div>
-      <div>
-        <Button variant="outlined">skip</Button>
+      <div className={styles['message-button']}>
+        <Button
+          variant="contained"
+          style={{ maxWidth: '120px', maxHeight: '40px', minWidth: '120px', minHeight: '40px' }}>
+          say hi
+        </Button>
+      </div>
+      <div className={styles['skip-button']}>
+        <Button
+          variant="outlined"
+          style={{ maxWidth: '120px', maxHeight: '40px', minWidth: '120px', minHeight: '40px' }}>
+          skip
+        </Button>
       </div>
     </Dialog>
   );
