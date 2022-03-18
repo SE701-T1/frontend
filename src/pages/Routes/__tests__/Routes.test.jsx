d6c6fce15d0623aa0d23d3018b4646ecb1668147
Shrey Tailor <39754888+shreytailor@users.noreply.github.com>
2022-03-18 17:42:49 +1300

    Updated the Router Navigation test.
    - This test implicitely tests the usage of Outlet.

--- src/pages/Routes/__tests__/Routes.test.jsx
+++ src/pages/Routes/__tests__/Routes.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
+import { AuthContext } from '../../../context/authContext';
 
 import Routes from '../Routes';
 
@@ -9,9 +10,12 @@ describe('Router Navigation', () => {
   test('should display Dashboard on root directory', () => {
     const history = createMemoryHistory();
     render(
-      <BrowserRouter history={history}>
-        <Routes />
-      </BrowserRouter>,
+      <AuthContext.Provider value={{ authenticated: true }}>
+        <BrowserRouter history={history}>
+          <Routes />
+        </BrowserRouter>
+        ,
+      </AuthContext.Provider>,
     );
     expect(screen.getByText('Welcome to the Dashboard')).toBeInTheDocument();
   });
