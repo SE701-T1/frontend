caf8eaec2b271e5a3495d35ca81dcf8cc8771850
uoa-lzha989 <68832471+uoa-lzha989@users.noreply.github.com>
2022-03-19 00:35:32 +1300

    Adjustments for unit test rendering Package lock changes

--- src/components/PopUp/PopUp.jsx
+++ src/components/PopUp/PopUp.jsx
@@ -4,14 +4,14 @@ import PeopleIcon from '@mui/icons-material/People';
 import styles from './PopUp.module.css';
 import ProfileBadge from '../ProfileBadge/ProfileBadge';
 
-function PopUp({ name, buddyNumber, open }) {
+function PopUp({ name, buddyNumber, open, size }) {
   const sharedCourses = ['course1', 'course2'];
   const courses = sharedCourses.join(', ');
 
   return (
-    <Dialog open={open} fullWidth>
+    <Dialog open={open} fullWidth data-testid='popup'>
       <div className={styles.icon}>
-        <ProfileBadge size="250px" marginTop="20px" />
+        <ProfileBadge name={name} size={size}/>
       </div>
       <div className={styles.title}>
         <h1> {name} </h1>
@@ -20,20 +20,20 @@ function PopUp({ name, buddyNumber, open }) {
         <Typography>Also takes {courses.toUpperCase()}</Typography>
       </div>
       <div className={styles.buddies}>
-        <PeopleIcon style={{ marginRight: '5px' }} />
+        <PeopleIcon sx={{ marginRight: '5px' }} />
         {buddyNumber} Buddies
       </div>
       <div className={styles['message-button']}>
         <Button
           variant="contained"
-          style={{ maxWidth: '120px', maxHeight: '40px', minWidth: '120px', minHeight: '40px' }}>
+          sx={{ maxWidth: '120px', maxHeight: '40px', minWidth: '120px', minHeight: '40px' }}>
           say hi
         </Button>
       </div>
       <div className={styles['skip-button']}>
         <Button
           variant="outlined"
-          style={{ maxWidth: '120px', maxHeight: '40px', minWidth: '120px', minHeight: '40px' }}>
+          sx={{ maxWidth: '120px', maxHeight: '40px', minWidth: '120px', minHeight: '40px' }}>
           skip
         </Button>
       </div>
