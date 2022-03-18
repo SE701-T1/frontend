ce77b0fa4f4dcdd33e2ff150eeb595bf85711fea
KirstyGong <66720955+KirstyGong@users.noreply.github.com>
2022-03-18 19:13:31 +1300

    Pairing page (#86)
    * feat: pairing page
    
    * feat: remove unneccessary css
    
    * test: setting up jest mock
    
    * test for pairing screen
    
    * fix formating and tests
    
    * doc: remove unneccesaary comments
    
    * fix: remove console log and changed to use prev
    
    * doc: commenting
    
    * doc: remove uncommented code

--- src/pages/Routes/Routes.jsx
+++ src/pages/Routes/Routes.jsx
@@ -4,13 +4,14 @@ import { Route, Routes as Switch } from 'react-router-dom';
 import Example from '../../components/Example';
 import Nav from '../../components/NavBar';
 import Dashboard from '../Dashboard';
+import Pairing from '../Pairing/Pairing';
 
 function Routes() {
   return (
     <Nav>
       <Switch>
         <Route path="/courses" element={<Example />} />
-        <Route path="/find-matches" element={<Example />} />
+        <Route path="/find-matches" element={<Pairing />} />
         <Route path="/chat" element={<Example />} />
         <Route path="/accounts" element={<Example />} />
         <Route path="/" element={<Dashboard />} />
