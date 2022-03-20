54c9ea6eacc58aaeef8ab7811b1e30d04d79d746
Shrey Tailor <shreytailor@canva.com>
2022-03-20 14:47:03 +1300

    Fixed the basic test for Settings component. (#126)

--- src/pages/Settings/__tests__/Settings.test.jsx
+++ src/pages/Settings/__tests__/Settings.test.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import Settings from '../Settings';
 
 describe('Render Login Button', () => {
   test('should display the Toggle text', () => {
-    render(<Settings />);
-
-    expect(screen.getByText('ON')).toBeInTheDocument();
+    const tree = renderer.create(<Settings />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
-    
 });
