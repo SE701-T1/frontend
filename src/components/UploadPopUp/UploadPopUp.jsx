1abc0bde40e1fe8d50f3d1e80d030cf23b306e9f
yuchendong <68779035+yuchendong13@users.noreply.github.com>
2022-04-01 15:48:16 +1300

    File # restriction, submit button, error messages

--- src/components/UploadPopUp/UploadPopUp.jsx
+++ src/components/UploadPopUp/UploadPopUp.jsx
@@ -5,6 +5,7 @@ import styles from './UploadPopUp.module.css';
 
 export default function UploadPopUp({ open, close }) {
   const [file, setFile] = useState([]);
+  const [flag, setFlag] = useState();
 
   function deleteFile(e) {
     const s = file.filter((item, index) => index !== e);
@@ -17,6 +18,14 @@ export default function UploadPopUp({ open, close }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if(file.length < 1) {
+      setFlag('true');
+    } else if(file.length > 1) {
+      setFlag('true');
+    } else {
+      setFlag('false');
+      close();
+    }
     /** need to add submit fucntion code here */
   };
 
@@ -51,6 +60,7 @@ export default function UploadPopUp({ open, close }) {
                     maxHeight: '40px',
                     minWidth: '120px',
                     minHeight: '40px',
+                    marginLeft: '10px'
                   }}
                   type="button"
                   onClick={() => deleteFile(index)}>
@@ -60,13 +70,30 @@ export default function UploadPopUp({ open, close }) {
             ))}
         </div>
       </form>
-
+      
+      <div className={styles['upload-errors']}>
+        {file.length < 1 &&
+          flag==='true' &&
+          'Please upload a file'
+        }
+        {file.length > 1 &&
+          flag==='true' &&
+          handleSubmit &&
+          'Only 1 file allowed'}
+      </div>
+      
       <div className={styles['close-button']}>
         <Button
           variant="outlined"
-          sx={{ maxWidth: '120px', maxHeight: '40px', minWidth: '120px', minHeight: '40px' }}
+          sx={{ maxWidth: '120px', maxHeight: '40px', minWidth: '120px', minHeight: '40px', marginRight: '5px'}}
           onClick={close}>
           Close
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ maxWidth: '120px', maxHeight: '40px', minWidth: '120px', minHeight: '40px', marginLeft: '5px' }}
+          onClick={handleSubmit}>
+          Submit
         </Button>
       </div>
     </Dialog>
