f0fd5725f85ebad1ad52dd5371eaa667c329bb93
yuchendong <68779035+yuchendong13@users.noreply.github.com>
2022-04-02 15:33:30 +1300

    File upload acceptance
    - Only 1 file can be uploaded at a time as it's a timetable
    - Only correct calendar file formats are allowed

--- src/components/UploadPopUp/UploadPopUp.jsx
+++ src/components/UploadPopUp/UploadPopUp.jsx
@@ -4,29 +4,22 @@ import UploadIcon from '@mui/icons-material/Upload';
 import styles from './UploadPopUp.module.css';
 
 export default function UploadPopUp({ open, close }) {
-  const [file, setFile] = useState([]);
-  const [flag, setFlag] = useState();
+  const [file, setFile] = useState(undefined);
 
-  function deleteFile(e) {
-    const s = file.filter((item, index) => index !== e);
-    setFile(s);
+  function deleteFile() {
+    setFile(undefined);
   }
 
   const handleOnChange = (e) => {
-    setFile([...file, ...e.target.files]);
+    if(e.target.files[0]){
+      setFile(e.target.files[0]);
+    }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(file.length < 1) {
-      setFlag('true');
-    } else if(file.length > 1) {
-      setFlag('true');
-    } else {
-      setFlag('false');
-      close();
-    }
-    /** need to add submit fucntion code here */
+    /* For issue #48: connect to backend */
+    close();
   };
 
   return (
@@ -43,44 +36,30 @@ export default function UploadPopUp({ open, close }) {
             sx={{ maxHeight: '40px', minWidth: '120px', minHeight: '40px' }}>
             <UploadIcon />
             Upload File
-            <input type="file" onChange={handleOnChange} hidden />
+            <input type="file" onChange={handleOnChange} hidden accept=".ical,.ics,.ifb,.icalendar" id="fileUpload"/>
           </Button>
         </div>
 
         <div className={styles['form-group-preview']}>
-          {file != null &&
-            file.length > 0 &&
-            file.map((item, index) => (
-              <div key={item}>
-                {item.name}
-                <Button
-                  variant="outlined"
-                  sx={{
-                    maxWidth: '120px',
-                    maxHeight: '40px',
-                    minWidth: '120px',
-                    minHeight: '40px',
-                    marginLeft: '10px'
-                  }}
-                  type="button"
-                  onClick={() => deleteFile(index)}>
-                  Delete
-                </Button>
-              </div>
-            ))}
+          {
+           file !== undefined &&
+           <><div>{file.name}</div> 
+           <Button
+            variant="outlined"
+            sx={{
+              maxWidth: '120px',
+              maxHeight: '40px',
+              minWidth: '120px',
+              minHeight: '40px',
+              marginLeft: '10px'
+            }}
+            type="button"
+            onClick={() => deleteFile()}>
+            Delete
+            </Button></>
+          }
         </div>
       </form>
-      
-      <div className={styles['upload-errors']}>
-        {file.length < 1 &&
-          flag==='true' &&
-          'Please upload a file'
-        }
-        {file.length > 1 &&
-          flag==='true' &&
-          handleSubmit &&
-          'Only 1 file allowed'}
-      </div>
       
       <div className={styles['close-button']}>
         <Button
